aacfd2545e37bc3275814df0addb02cc1cdc9d4c
Christoph Biering <the.chryb@icloud.com>
2017-01-15 23:57:14 +0100

    * Added view controllers for the several views * Update Main.storyboard * Added socket.io pod

--- SCC16iOSClient/Pods/Target Support Files/Pods-SCC16iOSClient/Pods-SCC16iOSClient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SCC16iOSClient : NSObject
-@end
-@implementation PodsDummy_Pods_SCC16iOSClient
-@end
---
+++ SCC16iOSClient/SCC16iOSClient/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherViewController.swift
+//  SCC16iOSClient
+//
+//  Created by Chryb on 15.01.17.
+//  Copyright © 2017 Chryb. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
